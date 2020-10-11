91b57ad4fb6c8fd2de96ea91fa9ed82e2b1043b4
NIk <nikitalux01@gmail.com>
2020-10-11 16:57:29 +0300

    first

---
+++ 09.10.2020/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.body.querySelector('#current').parentNode);
+console.log(document.body.querySelector('#current').parentElement);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+
+
+
+
+
---
+++ Course2/week5/Test task.js
@@ -0,0 +1,37 @@
+//1st task
+let x,y,z,w,s,a,max,min;
+function candies(x,y,z,w){
+    if(x>y){
+        max=x;
+    }else
+    {
+        if(y>z){
+            max=y;
+        }
+        else
+        {
+            max=z;
+        }
+    }
+
+    if(x<y){
+        min=x;
+    }else
+    {
+        if(y<z){
+            min=y;
+        }
+        else
+        {
+            min=z;
+        }
+    }
+
+
+    for(i=min;i<=max;i++)
+        for(m=min;m<=max;m++)
+            for(n=min;n<=max;n++){
+
+            }
+
+}

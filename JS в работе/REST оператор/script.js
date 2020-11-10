bacc607ce303b3afb77dfd2eb5dce6116dbdb276
NIk <nikitalux01@gmail.com>
2020-11-10 16:28:51 +0200

    10.11

--- JS в работе/REST оператор/script.js
+++ JS в работе/REST оператор/script.js
@@ -1,24 +1,14 @@
 'use strict';
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-}
-
-User.prototype.exit = function() {
-    console.log(`User ${this.name} exit`);
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
 };
 
 
+log('basic', 'rest', 'operator', 'usage');
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
-ivan.exit();
-ivan.hello();
-alex.hello();
-console.log(ivan);
-console.log(alex);
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
